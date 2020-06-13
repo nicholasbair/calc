3d1ad5b94c116ff61e07bb5a6bbc16aa17f51da2
nicholasbair <bairinc@gmail.com>
2020-06-13 10:14:50 -0600

    Add auth, closes #1.

--- calc.py
+++ calc.py
@@ -1,7 +1,22 @@
 import falcon
 import pandas as pd
 
-class ema(object):
+class AuthMiddleware(object):
+
+  def process_request(self, req, resp):
+    token = req.get_header('Authorization')
+
+    if token is None:
+      raise falcon.HTTPUnauthorized('Unauthorized')
+
+    if not self._token_is_valid(token):
+      raise falcon.HTTPUnauthorized('Unauthorized')
+
+  def _token_is_valid(self, token):
+    token == os.environ.get('CALC_TOKEN')
+
+
+class Ema(object):
 
   def on_post(self, req, resp):
     df_test = pd.DataFrame(req.media['data'])
@@ -10,8 +25,10 @@ class ema(object):
     resp.status = falcon.HTTP_200
     resp.body = (str(df_test_ewma.values.tolist()[-1][0]))
 
-app = falcon.API()
+app = falcon.API(middleware=[
+  AuthMiddleware()
+])
 
-ema = ema()
+ema = Ema()
 
 app.add_route('/ema', ema)
